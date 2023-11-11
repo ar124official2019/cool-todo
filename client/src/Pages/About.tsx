73f5293a1432b4c5238f4d091d401abffccf4e70
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2023-11-11 18:35:35 +0500

    Unify

--- client/src/Pages/About.tsx
+++ client/src/Pages/About.tsx
@@ -1,4 +1,9 @@
 
 export function About() {
-  return <div>This is a very cute Todo App!</div>
+  return (
+    <div className="rounded rounded-lg bg-blue-300 p-4">
+      <h1 className="font-bold text-center uppercase text-2xl">About</h1>
+      <div className="text-center my-2">This is a very cool todo application!</div>
+    </div>
+  );
 }
