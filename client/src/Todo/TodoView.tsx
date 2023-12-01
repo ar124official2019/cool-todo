f02f5d659cfc61341347aaa33c2a03ac7203c1ce
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2023-12-01 19:31:31 +0500

    Better actions

--- client/src/Todo/TodoView.tsx
+++ client/src/Todo/TodoView.tsx
@@ -4,14 +4,17 @@ import { useAppDispatch } from "../store";
 import { deleteTodo, selectTodo } from "../store/todo.slice";
 import { useHttpDelete } from "../types/http";
 import { AppModal } from "../Components/Modal";
-import { useState } from "react";
-import { Button, Card } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import { Button, Card, Badge } from "flowbite-react";
+import { HiCheck, HiPencil, HiTrash } from "react-icons/hi";
 
 export function TodoView(todo: ITodo) {
   const [toggleDelete, setToggleDelete] = useState(false);
   const httpDelete = useHttpDelete();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [mouseOver, setMouseOver] = useState(false);
+  const ref = useRef(null);
 
   const onEdit = () => {
     dispatch(selectTodo(todo));
@@ -23,53 +26,73 @@ export function TodoView(todo: ITodo) {
     dispatch(deleteTodo(todo.id));
   };
 
+  useEffect(() => {
+    if (ref?.current) {
+      const current = ref.current as HTMLDivElement;
+      
+      current.addEventListener("mouseover", () => {
+        setMouseOver(true);
+      });
+
+      current.addEventListener("mouseleave", () => {
+        setMouseOver(false);
+      });
+    }
+  }, [ref]);
+
   return (
-    <Card className="w-full h-full">
-      <b>{todo.todo}</b>
-      <p className="flex-1">{todo.description || "No Description"}</p>
+    <div className="w-full h-full" ref={ref}>
+      <Card className="w-full h-full relative">
+        {mouseOver && (
+          <div className="actions absolute top-4 right-4 flex flex-row gap-2">
+            <Badge
+              className="cursor-pointer hover:bg-slate-300"
+              icon={HiCheck}
+              color="light"
+            />
 
-      <div className="text-xs flex flex-col-reverse md:flex-row-reverse md:items-center">
-        <i>{new Date(todo.updatedAt).toLocaleString()}</i>
+            <Badge
+              className="cursor-pointer hover:bg-slate-300"
+              icon={HiPencil}
+              color="light"
+              onClick={onEdit}
+            />
 
-        <Button
-          size="small"
-          color="warning"
-          pill
-          className="px-4 mr-1 mb-1"
-          onClick={() => setToggleDelete(true)}
+            <Badge
+              className="cursor-pointer hover:bg-slate-300"
+              icon={HiTrash}
+              color="red"
+              onClick={() => setToggleDelete(true)}
+            />
+          </div>
+        )}
+
+        <b>{todo.todo}</b>
+        <p className="flex-1">{todo.description || "No Description"}</p>
+
+        <div className="text-xs flex flex-col-reverse md:flex-row-reverse md:items-center">
+          <i>{new Date(todo.updatedAt).toLocaleString()}</i>
+        </div>
+
+        <AppModal
+          title="Delete Todo"
+          visible={toggleDelete}
+          onClose={() => setToggleDelete(false)}
+          actions={[
+            <Button color="warning" onClick={() => onDelete()}>
+              Delete
+            </Button>,
+          ]}
         >
-          Delete
-        </Button>
-
-        <Button
-          size="small"
-          color="light"
-          pill
-          className="px-4 mr-1 mb-1"
-          onClick={onEdit}
-        >
-          Edit
-        </Button>
-      </div>
-
-      <AppModal
-        title="Delete Todo"
-        visible={toggleDelete}
-        onClose={() => setToggleDelete(false)}
-        actions={[
-          <Button color="warning" onClick={() => onDelete()}>
-            Delete
-          </Button>,
-        ]}
-      >
-        <p>
-          Do you really want to delete todo{" "}
-          <b>
-            <i>"{todo.todo}"</i>
-          </b>
-          ?
-        </p>
-      </AppModal>
-    </Card>
+          <p>
+            Do you really want to delete todo{" "}
+            <b>
+              <i>"{todo.todo}"</i>
+            </b>
+            ?
+          </p>
+        </AppModal>
+      </Card>
+    </div>
   );
 }
